011f067f2f289365e2dff78ad1969ce182a1376a
syip1 <44428342+syip1@users.noreply.github.com>
2021-10-30 20:02:45 +0100

    Update and rename ultrasonic.ino to ultrasonic.h

---
+++ ultrasonic.h
@@ -0,0 +1,20 @@
+int trigPin = 11;    // Trigger
+int echoPin = 12;    // Echo
+
+void setup() {
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+long get_ultrasonic() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  pinMode(echoPin, INPUT);
+  long duration = pulseIn(echoPin, HIGH);
+  long cm = (duration/2) / 29.1; 
+  return cm;
